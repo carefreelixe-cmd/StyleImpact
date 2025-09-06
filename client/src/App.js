7d860b1708cb47c768818b7fb9bbc160c3963411
Soumadip-cmd <soumadipsantra2004@gmail.com>
2025-09-06 16:43:39 +0530

    initialioze

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1 class="text-3xl font-bold text-red-200 underline">Hello world!</h1>
+    </>
+  );
+};
+
+export default App;
